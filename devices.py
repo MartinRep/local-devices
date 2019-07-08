dd12224522f7a57d6d49f65de7d5d9c22e3c8452
MartinRep <repickym@gmail.com>
2019-07-08 09:20:19 +0200

    Scheduler added

--- devices.py
+++ devices.py
@@ -1,18 +1,47 @@
 import socket
-from crontab import CronTab
 from netifaces import interfaces, ifaddresses, AF_INET
 import subprocess
 from flask import Flask, jsonify
+from threading import Timer
+
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer     = None
+        self.interval   = interval
+        self.function   = function
+        self.args       = args
+        self.kwargs     = kwargs
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
 
 app = Flask(__name__)
 nmap_output = 'devices.txt'
 devices = {}
 
-def cron_job(ip_mask):
-    cron = CronTab()
-    job = cron.new(command='nmap -sn {}'.format(ip_mask))  
-    job.minute.every(1)
-    cron.write()
+def nmap_job(ip_mask):
+    global devices
+    command = 'sudo nmap -sn {} > {}'.format(ip_mask, nmap_output)
+    print(command)
+    subprocess.run([command], shell = True)
+    try:
+        devices = update_connected_devs()
+    except IOError:
+        print("Error opening file: {}".format(nmap_output))
 
 def get_ip4_mask():
     iplist = [ifaddresses(face)[AF_INET][0]["addr"] for face in interfaces() if AF_INET in ifaddresses(face)]
@@ -20,7 +49,7 @@ def get_ip4_mask():
     return ip_mask
 
 def update_connected_devs():
-    global devices
+    con_devices = {}
     with open(nmap_output, 'r') as f:
         for line in f:
             # if 'Starting Nmap' in line:
@@ -31,18 +60,11 @@ def update_connected_devs():
             elif 'MAC Address:' in line:
                 mac_addr = str(line.split()[2])
                 host_name = ' '.join(line.split()[3:])[1:-1]
-                devices[mac_addr] = {'ip': ip_addr, 'host': host_name}
-    return devices
+                con_devices[mac_addr] = {'ip': ip_addr, 'host': host_name}
+    return con_devices
 
-# cron_job(get_ip4_mask)
-
-try:
-    update_connected_devs()
-    for dev, value in devices.items():
-        print(value['ip'], value['host'])
-except IOError:
-    print("Error opening file: {}".format(nmap_output))
-
+ip_mask = get_ip4_mask()
+nmap = RepeatedTimer(60, nmap_job, ip_mask)
 
 @app.route('/devices', methods=['GET'])
 def get_devices():
